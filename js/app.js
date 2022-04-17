e40a190c23cdb5548cbda11c54f1071ff749a607
Elizabeth <elisha.elizabeth@gmail.com>
2022-04-17 14:44:19 -0700

    made final updates to About Me page

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 'use strict';
 
+let score = 0;
+
 let aboutmeVisitor = prompt('Welcome! What is your name?');
-// console.log('User\'s name is ' + aboutmeVisitor);
 alert('Hello ' + aboutmeVisitor + ', do you want to play a guessing game about me? Please enter each response with a yes or no.');
 
+// question #1
+// function - input, processing, output. The prompt below is our input.
 function q1 (){
   let questionOne = prompt('Do I live in Seattle?').toLowerCase();
-  // console.log('User\'s response to question 1 was ' + questionOne);
 
   if(questionOne === 'yes' || questionOne === 'y'){
+    score = score + 1;
     alert('Yes, that is correct!');
   } else if (questionOne === 'no' || questionOne === 'n') {
     alert('That is incorrect. I do live in Seattle.');
@@ -19,11 +22,12 @@ function q1 (){
 
 q1();
 
+// question #2
 function q2 (){
   let questionTwo = prompt('Did I attend the University of Dayton?').toLowerCase();
-  // console.log('User\'s response to question 2 was ' + questionTwo);
 
   if(questionTwo === 'yes' || questionTwo === 'y'){
+    score = score + 1;
     alert('Yes, you are correct! I did attend UD, go flyers!');
   } else if (questionTwo === 'no' || questionTwo === 'n') {
     alert('Sorry, that is incorrect. I did attend UD.');
@@ -34,11 +38,12 @@ function q2 (){
 
 q2();
 
+// question #3
 function q3 (){
   let questionThree = prompt('Do I have a cat?').toLowerCase();
-  // console.log('User\'s response to question 3 was ' + questionThree);
 
   if(questionThree === 'no' || questionThree === 'n'){
+    score = score + 1;
     alert('That is correct! I do not own a cat, but I do have a cute dog!');
   } else if (questionThree === 'yes' || questionThree === 'y') {
     alert('That is incorrect. I have a dog named Duke.');
@@ -49,11 +54,12 @@ function q3 (){
 
 q3();
 
+//question #4
 function q4(){
   let questionFour = prompt('Is my dog Duke a chocolate labrador?').toLowerCase();
-  // console.log('User\'s response to question 4 was ' + questionFour);
 
   if(questionFour === 'no' || questionFour === 'n'){
+    score = score + 1;
     alert('Correct, he is a yellow labrador retriever.');
   } else if (questionFour === 'yes' || questionFour === 'y') {
     alert('No, Duke is not a chocolate labrador.');
@@ -64,11 +70,12 @@ function q4(){
 
 q4();
 
+//question #5
 function q5(){
   let questionFive = prompt('Am I excited to be coding?').toLowerCase();
-  // console.log('User\'s response to question 5 was ' + questionFive);
 
   if(questionFive === 'yes' || questionFive === 'y'){
+    score = score + 1;
     alert('Yes that\'s correct! Thanks for playing ' + aboutmeVisitor + '!');
   } else if (questionFive === 'no' || questionFive === 'n') {
     alert('Sorry, that is incorrect. Thanks for playing ' + aboutmeVisitor + '!');
@@ -79,13 +86,13 @@ function q5(){
 
 q5();
 
-// collaborated with Guy, David, Katharine, and Vinny on the below for lab03
+// question #6: collaborated with Guy, David, Katharine, and Vinny on the below for lab03
 function q6(){
   let correctAnswer = 12;
   for(let i = 0; i < 4; i++) {
     let questionSix = parseInt(prompt('Can you guess my favorite number?'));
-    //console.log(questionSix);
     if (questionSix === correctAnswer){
+      score = score + 1;
       alert (`${aboutmeVisitor}, that is correct!`);
       break;
     } else if (questionSix > correctAnswer){
@@ -100,12 +107,14 @@ function q6(){
 
 q6();
 
+// question #7
 function q7(){
   let statesAnswer = ['washington', 'california', 'illinois', 'ohio', 'south carolina'];
   for(let i = 0; i < 6; i++){
     let questionSeven = prompt('Name a state that I have lived in.').toLowerCase();
     for(let j = 0; j < statesAnswer.length; j++){
       if (questionSeven === statesAnswer[j]){
+        score = score + 1;
         alert(`${aboutmeVisitor}, that is correct!`);
         i = 6;
         break;
@@ -117,4 +126,4 @@ function q7(){
 
 q7();
 
-alert(`${aboutmeVisitor} you got X number of questions right!`);
+alert(`${aboutmeVisitor}, you got ${score} question(s) right!`);
